1c31a5b3cf399a4fa4ec8c2df35f51d785e9c88c
JoshGetsingerIPS <josh.getsinger@ipscorp.com>
2024-09-06 15:11:09 -0400

    trying to deploy to cloudflare

--- nextjs-dashboard/next.config.mjs
+++ nextjs-dashboard/next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    ppr: 'incremental',
+  }
+};
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
